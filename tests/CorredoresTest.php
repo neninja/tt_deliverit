8a5790792c0b61788628df2c56f32e4eff1a0aa6
Felipe Silva <felipe.dacs@gmail.com>
2021-07-12 03:29:33 -0300

    tests: os testes agr são obrigados a usarem migrations

--- tests/CorredoresTest.php
+++ tests/CorredoresTest.php
@@ -7,6 +7,12 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class CorredoresTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /**
+     * @group http
+     * @group db
+     */
     public function testDeveListarCorredores()
     {
         $response = $this->call('GET', '/corredores');
--- tests/ProvasTest.php
+++ tests/ProvasTest.php
@@ -7,6 +7,12 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ProvasTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /**
+     * @group http
+     * @group db
+     */
     public function testDeveListarProvas()
     {
         $response = $this->call('GET', '/provas');
