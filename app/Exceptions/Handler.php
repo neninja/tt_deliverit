e036a6bbb475b0ebba3562bb1b12bb869d034022
Felipe Silva <felipe.dacs@gmail.com>
2021-07-12 11:09:39 -0300

    fix: desativa log desnecessário

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,7 +38,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        parent::report($exception);
+        // parent::report($exception);
     }
 
     /**
