0c80b2f139d3693d72e2765aa730b0389cd8c7e9
Felipe Silva <felipe.dacs@gmail.com>
2021-07-12 02:47:27 -0300

    tests: add namespace

--- tests/ParticipacoesTest.php
+++ tests/ParticipacoesTest.php
@@ -1,10 +1,14 @@
 <?php
 
+namespace Tests;
+
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ParticipacoesTest extends TestCase
 {
+    use DatabaseMigrations;
+
     public function testDeveListarInscricoes()
     {
         $response = $this->call('GET', '/participacoes');
